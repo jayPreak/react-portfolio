5f5cf635a2dc4701c105b4991897d5020bdebea5
jayPreak <jayesh.preak2003@gmail.com>
2023-02-09 17:44:02 +0530

    componenets route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import "./Cursor/Cursor.css";
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import GlobalStyle from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import { Route, Switch } from "react-router-dom";
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
 
 export default function App() {
   const [mousePosition, setMousePosition] = useState({
@@ -55,7 +63,16 @@ export default function App() {
         animate={cursorVariant}
       />
       <GlobalStyle />
-      App File
+      <ThemeProvider theme={lightTheme}></ThemeProvider>
+      {/* App File */}
+
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/blog" component={BlogPage} />
+        <Route exact path="/work" component={WorkPage} />
+        <Route exact path="/skills" component={MySkillsPage} />
+      </Switch>
     </div>
   );
 }
